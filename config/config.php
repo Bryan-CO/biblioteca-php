2ceaa9945573fac7b68fe3f25dc2accc89286655
bryan <luisbryann080403@gmail.com>
2024-09-14 20:10:43 -0500

    feat: add config file for dbConnection

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+    class config{
+        public static $DB_USER = "postgres";
+        public static $DB_PASS = "rdbHjwMBMBbmMOMatScaLQAtmgLjbTrs";
+        public static $DB_HOST = "junction.proxy.rlwy.net";
+        public static $DB_PORT = 52259;
+        public static $DB_NAME = "sistema_biblioteca";
+    }
