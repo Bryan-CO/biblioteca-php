e19eae6c710df47b26b230f023014d5961332b36
bryan <luisbryann080403@gmail.com>
2024-09-21 00:08:20 -0500

    feat: add CORS in the index

--- index.php
+++ index.php
@@ -1,45 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+require_once 'config/config.php';
+require_once 'config/Router.php';
+require_once 'shared/ResponseModel.php';
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+// CORS
+header("Access-Control-Allow-Origin: *");
 
-<body>
-    <?php
-    require_once('config/config.php');
-    require_once('config/Router.php');
-    require_once('infrastructure/db/ClientPostgre.impl.php');
-    require_once('infrastructure/repositories/Libro.repository.impl.php');
-    require_once('infrastructure/services/Libro.service.impl.php');
-    require_once('infrastructure/controllers/LibroController.php');
-    
+// Métodos HTTP permitidos
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+
+// Encabezados permitidos
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 
-
-    Router::GET('/libro', function ($req) {
-        $db = new ClientPostgreSQL();
-        $repo = new LibroRepository($db);
-        $service = new LibroService($repo);
-        $controller = new LibroController($service);
-        
-        $autores = $controller->getLibros();
-        echo json_encode($autores);
-    });
-
-    
-    Router::GET('/libro/:id', function ($req) {
-        $db = new ClientPostgreSQL();
-        $repo = new LibroRepository($db);
-        $service = new LibroService($repo);
-        $controller = new LibroController($service);
-        
-        $autores = $controller->getLibroById(1);
-        echo json_encode($autores);
-    });
-    ?>
-</body>
-
-</html>
+// RUTAS
+require_once 'routes/LibroRoute.php';
