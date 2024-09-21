ba5f0e49aa1f36c8d56a71fef38dc71781db062a
bryan <luisbryann080403@gmail.com>
2024-09-21 00:12:17 -0500

    fix: off-Index Element in Path with Parameters Fixed

--- config/Router.php
+++ config/Router.php
@@ -1,53 +1,66 @@
 <?php
-    require_once('Request.php');
-    class Router{
-        private static Request $req;
+require_once 'Request.php';
+class Router
+{
+    private static Request $req;
 
-        public static function initializeRequest($params = []) {
-            self::$req = new Request($params);
-        }
+    public static function initializeRequest($params = [])
+    {
+        self::$req = new Request($params);
+    }
 
-        static function GET($url, $callback){
-            if(REQUEST_METHOD === 'GET' && self::isMatch($url)){
-                ob_clean();
-                header('Content-Type: application/json');
-                $callback(self::$req);
-                exit;
-            }
-        }
+    static function GET($url, $callback)
+    {
+        if (REQUEST_METHOD === 'GET' && self::isMatch($url)) {
+            ob_clean();
+            header("Access-Control-Allow-Origin: *");
 
-        static function POST($url, $callback){
-            if(REQUEST_METHOD === 'POST' && self::isMatch($url)){
-                ob_clean();
-                header('Content-Type: application/json');
-                $callback(self::$req);
-                exit;
-            }
-        }
-        
-        private static function isMatch($pathUrl){
-            
-            $param = self::containParameters($pathUrl);
-            if($param === null){
-                self::initializeRequest();
-                return $pathUrl === REQUEST_URL;
-            }
+            // Métodos HTTP permitidos
+            header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 
-            $subRuta = explode('/', $pathUrl);
-            $subReqRuta = explode('/', REQUEST_URL);
-            $params = [$param => $subReqRuta[2]];
-
-            self::initializeRequest($params);
-
-            return $subRuta[1] === $subReqRuta[1];
-        }
-        private static function containParameters($pathUrl){
-            $param = null;
-            foreach(explode('/', $pathUrl) as $rt){
-                if(str_starts_with($rt, ':')){
-                    $param = substr($rt, 1);
-                }
-            }
-            return $param;
+            // Encabezados permitidos
+            header("Access-Control-Allow-Headers: Content-Type, Authorization");
+            header('Content-Type: application/json');
+            $callback(self::$req);
+            exit;
         }
     }
+
+    static function POST($url, $callback)
+    {
+        if (REQUEST_METHOD === 'POST' && self::isMatch($url)) {
+            ob_clean();
+            header('Content-Type: application/json');
+            $callback(self::$req);
+            exit;
+        }
+    }
+
+    private static function isMatch($pathUrl)
+    {
+
+        $param = self::containParameters($pathUrl);
+        if ($param === null) {
+            self::initializeRequest();
+            return $pathUrl === REQUEST_URL;
+        }
+
+        $subRuta = explode('/', $pathUrl);
+        $subReqRuta = explode('/', REQUEST_URL);
+        if (count($subReqRuta) === count($subRuta)) {
+            $params = [$param => $subReqRuta[2]];
+            self::initializeRequest($params);
+        }
+        return $subRuta[1] === $subReqRuta[1];
+    }
+    private static function containParameters($pathUrl)
+    {
+        $param = null;
+        foreach (explode('/', $pathUrl) as $rt) {
+            if (str_starts_with($rt, ':')) {
+                $param = substr($rt, 1);
+            }
+        }
+        return $param;
+    }
+}
