f572e9e713a33601babea40e9b08b3388f7eea0f
bryan <luisbryann080403@gmail.com>
2024-09-21 22:11:57 -0500

    refactor: add Request type in endpoint

--- backend/routes/LibroRoute.php
+++ backend/routes/LibroRoute.php
@@ -2,13 +2,13 @@
 
 require_once CONTAINER . "LibroContainer.php";
 
-Router::GET('/libro', function ($req) {
+Router::GET('/libros', function ($req) {
     $libros = getController()->getLibros();
     ResponseModel::success($libros);
 });
 
 
-Router::GET('/libro/:id', function ($req) {
+Router::GET('/libros/:id', function (Request $req) {
     $libro = getController()->getLibroById($req->params['id']);
     ResponseModel::success($libro);
 });
