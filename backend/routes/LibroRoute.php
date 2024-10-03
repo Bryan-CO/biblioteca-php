ab558bf0229b57825b5e7775809b1dff5665361f
Eduardo Quea <eduardoquea3@gmail.com>
2024-10-02 21:17:21 -0500

    feat: add routes

--- backend/routes/LibroRoute.php
+++ backend/routes/LibroRoute.php
@@ -3,5 +3,7 @@
 require_once CONTAINER . "LibroContainer.php";
 
 Router::GET('/libros', fn() => getLibroController()->getLibros());
-Router::GET('/libros/:id', fn (Request $req) => getLibroController()->getLibroById($req));
+Router::GET('/libros/:id', fn(Request $req) => getLibroController()->getLibroById($req));
 Router::POST('/libros', fn(Request $req) => getLibroController()->addLibro($req));
+Router::PUT('/librosa/:id', fn(Request $req) => getLibroController()->editLibro($req));
+Router::DELETE('/librosa/:id', fn(Request $req) => getLibroController()->deleteLibro($req));
