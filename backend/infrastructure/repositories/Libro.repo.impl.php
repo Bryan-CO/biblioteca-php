0fdd2c1b13177808ee0fbd32c954376140ab6699
Eduardo Quea <eduardoquea3@gmail.com>
2024-10-02 21:17:11 -0500

    feat: add edit and format document

--- backend/infrastructure/repositories/Libro.repo.impl.php
+++ backend/infrastructure/repositories/Libro.repo.impl.php
@@ -1,38 +1,54 @@
 <?php
-    require_once I_REPOSITORY . "Libro.repository.php";
-    require_once I_SERVICE . "ClientDB.php";
-    require_once MAPPER . 'LibroMapper.php';
-    class LibroRepository implements ILibroRepository{
-        private $dbClient;
-        public function __construct(IClientDB $dbClient){
-            $this->dbClient = $dbClient;
-        }
-        public function getAll(){
-            return pgToLibro($this->dbClient->executeProcedure("getlibros", TypeProcedure::FUNCTION));
-        }
-        public function getById($id){
-            return $this->dbClient->executeProcedure("getlibro", TypeProcedure::FUNCTION, [$id]);
-        }
-        public function add(Libro $libro){
-            return $this->dbClient->executeProcedure("addlibro", TypeProcedure::FUNCTION, [
-                $libro->getSerialNumber(),
-                $libro->getNombre(),
-                $libro->getSinopsis(),
-                $libro->getAutor()->getIdAutor(),
-                $libro->getIdioma()->getIdIdioma(),
-                $libro->getEditorial()->getIdEditoral(),
-                $libro->getCategoria()->getIdCategoria(),
-                $libro->getSubgenero()->getIdSubgenero(),
-                $libro->getAnio(),
-                $libro->getUnidades(),
-                $libro->getCantPaginas(),
-                $libro->getUrlImagen()
-            ]);
-        }
-        public function edit(Libro $libro){
-            
-        }
-        public function delete($id){
-            
-        }
-    }
+require_once I_REPOSITORY . "Libro.repository.php";
+require_once I_SERVICE . "ClientDB.php";
+require_once MAPPER . 'LibroMapper.php';
+class LibroRepository implements ILibroRepository
+{
+  private $dbClient;
+  public function __construct(IClientDB $dbClient)
+  {
+    $this->dbClient = $dbClient;
+  }
+  public function getAll()
+  {
+    return pgToLibro($this->dbClient->executeProcedure("getlibros", TypeProcedure::FUNCTION));
+  }
+  public function getById($id)
+  {
+    return $this->dbClient->executeProcedure("getlibro", TypeProcedure::FUNCTION, [$id]);
+  }
+  public function add(Libro $libro)
+  {
+    return $this->dbClient->executeProcedure("addlibro", TypeProcedure::FUNCTION, [
+      $libro->getSerialNumber(),
+      $libro->getNombre(),
+      $libro->getSinopsis(),
+      $libro->getAutor()->getIdAutor(),
+      $libro->getIdioma()->getIdIdioma(),
+      $libro->getEditorial()->getIdEditoral(),
+      $libro->getCategoria()->getIdCategoria(),
+      $libro->getSubgenero()->getIdSubgenero(),
+      $libro->getAnio(),
+      $libro->getUnidades(),
+      $libro->getCantPaginas(),
+      $libro->getUrlImagen()
+    ]);
+  }
+  public function edit(Libro $libro)
+  {
+    return $this->dbClient->executeProcedure("editlibro", TypeProcedure::FUNCTION, [
+      $libro->getNombre(),
+      $libro->getSinopsis(),
+      $libro->getAutor()->getIdAutor(),
+      $libro->getIdioma()->getIdIdioma(),
+      $libro->getEditorial()->getIdEditoral(),
+      $libro->getCategoria()->getIdCategoria(),
+      $libro->getSubgenero()->getIdSubgenero(),
+      $libro->getAnio(),
+      $libro->getUnidades(),
+      $libro->getCantPaginas(),
+      $libro->getUrlImagen(),
+    ]);
+  }
+  public function delete($id) {}
+}
